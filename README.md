1353d5bdb8dfc3e212c61cb17c852f6562a4275b
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2023-02-08 17:08:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# hal-publication-list
-Utilisation de l'API HAL pour générer une liste de publications sur une page web. 
+# Liste publications HAL
+![GitHub](https://img.shields.io/github/license/tschmoderer/hal-publication-list?color=blue)
+
+Ce dépôt contient une page web minimale qui permet d'intégrer facilement une bibliographie HAL dans un site web. Le code utilise l'API HAL pour récupérer les données puis les présentent avec style imitant celui du CV HAL.
+
+[TOC]
+
+## Aperçu du résultat
+
+![Résultat](./img/result.png)
+
+## Tutoriel
+
+Pour intégrer une bibliographie HAL dans un site munissez vous de l'[idHal](https://doc.archives-ouvertes.fr/identifiant-auteur-idhal-cv/) du chercheur(se) en question et téléchargez les fichiers de ce dépôt git. 
+
+1. A l'endroit où vous souhaitez intégrer la bibliographie insérez le nœud HTML suivant 
+
+   ```html
+   <div id="publi-hal-all"></div>
+   ```
+
+2. Dans le <head> de votre page, insérez le code suivant 
+
+   ```html
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="stylesheet" href="hal-style.css">
+   ```
+
+   qui charge les ressources CSS;
+
+3. (*Optionnel*) Si certain titres d'articles contiennent des maths , pour qu'il soient correctement formatés, ajoutez dans le <head> de votre page le code suivant
+
+   ```html
+   <script>
+       MathJax = {
+           tex: {
+               inlineMath: [['$', '$'], ['\\(', '\\)']],
+               processEscapes: true,
+               tags: "all"
+           },
+           svg: {
+               fontCache: 'global'
+           },
+           loader: {
+               load: ['[tex]/html']
+           }
+       };
+   </script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.min.js" integrity="sha512-EtUjpk/hY3NXp8vfrPUJWhepp1ZbgSI10DKPzfd+3J/p2Wo89JRBvQIdk3Q83qAEhKOiFOsYfhqFnOEv23L+dA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   ```
+
+4. A la fin du <head> de votre page HTML, ajoutez le code suivant en configurant l'idHal cible et la liste des types de publications souhaitées (voir plus bas pour les types pris en charge)
+
+   ```html
+   <script type="text/javascript">
+       // idHal personalisé
+       const idhal = "custom-idhal";
+       // Liste des keywords de type de publications souhaitées
+       const publiList = ["THESE", "ART", "UNDEFINED", "COMM"]
+   </script>
+   ```
+
+*NB*: Les différentes sections de publications apparaissent dans l'ordre de la liste définies dans la variable *publiList*.
+
+## Liste des types de publications HAL
+
+Le tableau suivant donne les keywords a utiliser dans la variable *publiList* pour obtenir. La dernière colonne indique si le type est pris en charge: 
+
+| Keyword     | Description | Pris en charge |
+| :---------: | ----------- | :------------: |
+| ART         | Article de journal | :heavy_check_mark: |
+| COMM        | Communication dans un congrès | :heavy_check_mark: |
+| COUV        | ? | :x: |
+| THESE       | Thèse | :heavy_check_mark: |
+| OUV         | ? | :x: |
+| UNDEFINED   | Preprint, document de travail | :heavy_check_mark: |
+| REPORT      | ? | :x: |
+| OTHER       | ? | :x: |
+| MEM         | ? | :x: |
+| IMG         | Image | :x: |
+| POSTER      | ? | :x: |
+| ISSUE       | ? | :x: |
+| PROCEEDINGS | ? | :x: |
+| HDR         | Habilitation à diriger des recherche | :x: |
+| NOTICE      | ? | :x: |
+| PATENT      | Brevet | :x: |
+| BLOG        | ? | :x: |
+| REPORT_LABO | ? | :x: |
+| VIDEO       | Vidéo | :x: |
+| REPORT_MAST | ? | :x: |
+| LECTURE     | ? | :x: |
+| REPORT_LPRO | ? | :x: |
+| REPORT_LICE | ? | :x: |
+| TRAD        | ? | :x: |
+| SOFTWARE    | Logiciel | :x: |
+| CREPORT     | ? | :x: |
+| PRESCONF    | ? | :x: |
+| REPORT_DOCT | ? | :x: |
+| REPORT_ETAB | ? | :x: |
+| MAP | ? | :x: |
+| SON | ? | :x: |
+| REPORT_FORM | ? | :x: |
+| REPORT_GMAST | ? | :x: |
+| OTHERREPORT | ? | :x: |
+| NOTE | ? | :x: |
+| SYNTHESE | ? | :x: |
+| REPORT_FPROJ | ? | :x: |
+| REPORT_GLICE | ? | :x: |
+| REPACT | ? | :x: |
+| MEMLIC | ? | :x: |
+| REPORT_RFOINT | ? | :x: |
+| REPORT_COOR | ? | :x: |
+| ETABTHESE | ? | :x: |
+| REPORT_RETABINT | ? | :x: |
+| MANUAL | ? | :x: |
+| DOUV | ? | :x: |
+
+## Ajouter le support de nouveaux types
+
+Dans le fichier *hal-script.js* complétez la variable *hal_helpers* avec un nouveau type de publication : 
+
+```json
+"TYPE": {
+    icon: "icon class",
+    title_en: "titre de la section pour ce type de publication"
+}
+```
+
+Les icônes doivent être dans la liste des icônes gratuites de [fontawesome](https://fontawesome.com/icons). Merci de partager votre contribution soit en créant un pull-request sur ce dépôt, soit en envoyant votre nouvelle version par mail à [T. Schmoderer](mailto:timothee.schmoderer@insa-rouen.fr).
+
+## Limitations
+
+- Une seule section de publication par page est permise. 
---
+++ hal-script.js
@@ -0,0 +1,151 @@
+const hal_helpers = {
+    "THESE": {
+        icon: "fa-graduation-cap",
+        title_en: "Thesis",
+    },
+
+    "ART": {
+        icon: "fa-newspaper",
+        title_en: "Journal articles",
+    },
+
+    "UNDEFINED": {
+        icon: "fa-file-pen",
+        title_en: "Preprint",
+    },
+
+    "COMM": {
+        icon: "fa-microphone",
+        title_en: "Communications",
+    },
+};
+
+async function callHALAPI(type) {
+    try {
+        const url = "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:" + idhal + "&wt=json"
+        const response = await fetch(url + "&fl=title_s,halId_s,citationRef_s,defenseDateY_i,journalTitle_s,authFullName_s ,fileMain_s, thumbId_i&fq=docType_s:" + type);
+        const data = await response.json();
+        //const publications = data.response.docs;
+        return data.response.docs;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function initialHTML(type) {
+    var str = "<div id='hal-" + type + "-main'>" +
+        "<div class='hal-list' id='hal-" + type + "'>" +
+        "<!--HAL " + type + " BUTTON-->" +
+        "<button data-target='#" + type + "' class='hal-btn' onclick='collapse(this)'>" +
+        "<i class='hal-icon fa-solid " + hal_helpers[type]["icon"] + "'></i>" +
+        "<a id='hal-" + type + "-card-title'></a>" +
+        "<i class='hal-icon icon-drop_down fa-solid fa-caret-down'></i>" +
+        "</button>" +
+        "<!--END HAL " + type + " BUTTON-->" +
+        "<div class='hal-spinner' id='hal-" + type + "-spinner'>" +
+        "<div class='lds-ellipsis'>" +
+        "<div></div><div></div><div></div><div></div>" +
+        "</div>" +
+        "</div>" +
+        "<!--HAL " + type + " CONTENT-->" +
+        "<div class='hal-content' id='" + type + "'>" +
+        "<table class='hal-results-table hal-table-white'>" +
+        "<tbody id='hal-" + type + "-table'>" + "</tbody>" +
+        "</table>" + " </div>" + "</div>" +
+        "<!--HAL " + type + " CONTENT-->" +
+        "</div>" +
+        "</div>";
+    return str;
+}
+
+function genListPubli(type) {
+    callHALAPI(type).then(data => {
+        // Set title 
+        document.getElementById("hal-" + type + "-card-title").innerText = hal_helpers[type]["title_en"] + " (" + data.length + ")";
+
+        // Complete list 
+        var tab = document.getElementById("hal-" + type + "-table");
+        for (const p of data) {
+            console.log(p);
+            var str = ""
+            str = str + "<tr>";
+            if (p.thumbId_i) {
+                str += "<td class='d-sm-table-cell'>" +
+                    "<a href=" + p.fileMain_s + ">" +
+                    "<div class='hal-media d-sm-block'>" +
+                    "<img src='https://thumb.ccsd.cnrs.fr/" + p.thumbId_i + "/thumb' alt='Image document'>" +
+                    "</div>" +
+                    "</a>" +
+                    "</td>";
+            }
+            str += "<td style='width: 100%' class='hal-title'>" +
+                "<a href='https://hal.science/" + p.halId_s + "' target='_blank'>" +
+                "<h3 class='title-results'>" +
+                p.title_s[0] +
+                "</h3>" +
+                "<span class='authors-results'>";
+
+            let nbit = p.authFullName_s.length;
+            for (const a of p.authFullName_s) {
+                str = str +
+                    "<a href='https://hal.science/search/?q=*&authFullName_s=" +
+                    a + "' alt='Documents de l\'auteur' target='_blank'>" +
+                    a + "</a>";
+                if (--nbit) {
+                    str = str + " ; ";
+                };
+            };
+
+            str = str + "</span>" +
+                "</a>";
+            str = str + "<br>";
+            str = str +
+                "<div class='citation-results'>" +
+                p.citationRef_s +
+                "</div>" +
+                "</td>" +
+                " </tr>";
+            tab.innerHTML += str;
+        };
+
+
+        // Remove loader
+        document.getElementById("hal-" + type + "-spinner").style.display = "none";
+        document.getElementById("hal-" + type).style.display = "block";
+
+        // Update mathjax
+        MathJax.typeset([document.getElementById(type)]);
+    }).catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(idhal);
+    console.log(publiList);
+
+    const hal_div = document.getElementById("publi-hal-all");
+
+    if (hal_div.length === 0) {
+        console.log("No Hal on this page");
+        return 0;
+    }
+
+    for (const p of publiList) {
+        // console.log("Add initial html for " + p);
+        // initialisation 
+        hal_div.innerHTML += initialHTML(p);
+
+        // generate publi list 
+        genListPubli(p);
+    }
+});
+
+function collapse(elem) {
+    const elem2 = document.getElementById(elem.getAttribute("data-target").replace('#', ''));
+    if (elem2.style.display === "none") {
+        elem2.style.display = "block";
+        elem.querySelector(".icon-drop_down").classList.remove("fa-rotate-by");
+    } else {
+        elem2.style.display = "none";
+        elem.querySelector(".icon-drop_down").classList.add("fa-rotate-by");
+    }
+}
