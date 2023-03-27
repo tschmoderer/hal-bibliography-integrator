24cc0d4ff92be00d35c20f3a41be6abc0985c053
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2023-03-27 12:44:50 +0200

    add wordcloud support

--- hal-script.js
+++ hal-script.js
@@ -1,3 +1,10 @@
+// Point d'entrée de l'API
+const hal_api_url = "https://api.archives-ouvertes.fr/search";
+const hal_api_default_param = {
+    q: "authIdHal_s:" + idhal, 
+    wt: "json",
+};
+
 const hal_helpers = {
     "THESE": {
         "icon": "fa-graduation-cap",
@@ -30,51 +37,139 @@ const hal_helpers = {
     }
 };
 
-async function callHALAPI(type) {
+async function callHALAPI(parameters) {
     try {
-        var url = "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:" + idhal + "&wt=json"
-        url    += "&fl=title_s,halId_s,citationRef_s,defenseDateY_i,journalTitle_s,authFullName_s,publicationDate_tdate,fileMain_s, thumbId_i&fq=docType_s:";
-        url    += type;
-        url    += "&sort=publicationDate_tdate desc";
+        // Construction des paramètres de la recherche
+        const param = new URLSearchParams(parameters);
+        for (const [key, value] of Object.entries(hal_api_default_param)) {
+            param.append(key, value);
+        }
+        
+        // Instanciation de l'url 
+        var url = new URL(hal_api_url);
+        url.search = param.toString();
         if ((typeof halDebug !== "undefined") && (halDebug)) {
             console.dir(url);
         }
+
+        // Appel de l'api
         const response = await fetch(url);
         const data     = await response.json();
         return data.response.docs;
+
     } catch (error) {
+        console.log("Error in call HAL API on url : " + url.toString())
         console.error(error);
     }
 }
 
+function HALwordcloud(hal_wordcloud_div) {
+    const param = {
+        fl: "en_keyword_s",
+    };
+
+    callHALAPI(param).then(data => {
+
+        var wordcloud_container = document.createElement("div");
+        wordcloud_container.classList = "hal-cloud-word";
+
+        if ((typeof halDebug !== "undefined") && (halDebug)) {
+            console.log(data);
+        }
+
+        // Extract the English keywords from the publications
+        const keywords = data.flatMap(doc => doc.en_keyword_s).filter(keyword => keyword !== undefined && keyword !== null && keyword !== "");
+        
+        // Create a word frequency object
+        const wordFreq = {};
+        keywords.forEach(k => wordFreq[k] ? wordFreq[k]++ : wordFreq[k] = 1 );
+
+        // Map keyword freq in range 1 to 10
+        var max   = Object.entries(wordFreq).reduce((a, b) => a[1] > b[1] ? a : b)[1]
+        var min   = Object.entries(wordFreq).reduce((a, b) => a[1] < b[1] ? a : b)[1]
+
+        // Shuffle wordlist
+        var len    = Object.keys(wordFreq).length;
+        var suffle = Array.from(Array(len).keys())
+        suffle.sort(function(a, b){return 0.5 - Math.random()});
+
+        // Create word Cloud
+        for (let i = 0; i < len; i++) {
+            var word = Object.keys(wordFreq)[suffle[i]];
+            var freq = Object.values(wordFreq)[suffle[i]];
+            var new_freq = (9*freq+max-10*min)/(max-min);
+            wordcloud_container.appendChild(keywordElement(word, new_freq));
+        };
+
+        hal_wordcloud_div.appendChild(wordcloud_container);
+    });
+}
+
 function initialHTML(type) {
-    var str = "<div id='hal-" + type + "-main'>" +
-        "<div class='hal-list' id='hal-" + type + "'>" +
-        "<!--HAL " + type + " BUTTON-->" +
-        "<button data-target='#" + type + "' class='hal-btn' onclick='collapse(this)'>" +
-        "<i class='hal-icon fa-solid " + hal_helpers[type]["icon"] + "'></i>" +
-        "<a id='hal-" + type + "-card-title'>" + hal_helpers[type]["title_en"] + "</a>" +
-        "<i class='hal-icon icon-drop_down fa-solid fa-caret-down'></i>" +
-        "</button>" +
-        "<!--END HAL " + type + " BUTTON-->" +
-        "<div class='hal-spinner' id='hal-" + type + "-spinner'>" +
-        "<div class='lds-ellipsis'>" +
-        "<div></div><div></div><div></div><div></div>" +
-        "</div>" +
-        "</div>" +
-        "<!--HAL " + type + " CONTENT-->" +
-        "<div class='hal-content' id='" + type + "'>" +
-        "<table class='hal-results-table hal-table-white'>" +
-        "<tbody id='hal-" + type + "-table'>" + "</tbody>" +
-        "</table>" + " </div>" + "</div>" +
-        "<!--HAL " + type + " CONTENT-->" +
-        "</div>" +
-        "</div>";
-    return str;
+    var container = document.createElement("div");
+    container.classList = 'hal-list';
+    container.id = "hal-" + type;
+
+    var button = document.createElement("button");
+    button.classList = "hal-btn";
+    button.setAttribute("data-target", "#" + type);
+    button.setAttribute("onclick", "collapse(this)");
+
+    button.innerHTML = "<i class='hal-icon fa-solid " + hal_helpers[type]["icon"] + "'></i>"; 
+    button.innerHTML += "<a id='hal-" + type + "-card-title'>" + hal_helpers[type]["title_en"] + "</a>";
+    button.innerHTML += "<i class='hal-icon icon-drop_down fa-solid fa-caret-down'></i>";
+
+    container.appendChild(button);
+
+    var spinner = document.createElement("div");
+    spinner.classList = "hal-spinne";
+    spinner.id = "hal-" + type + "-spinner";
+
+    var ellipsis = document.createElement("div");
+    ellipsis.classList = "lds-ellipsis"; 
+    for (let i = 1; i <= 4; i++) {
+        ellipsis.appendChild(document.createElement("div"));
+    }
+
+    spinner.appendChild(ellipsis); 
+    container.appendChild(spinner);
+
+    var content = document.createElement("div"); 
+    content.classList = "hal-content"; 
+    content.id = type; 
+    content.innerHTML = "<table class='hal-results-table hal-table-white'><tbody id='hal-" + type + "-table'>" + "</tbody></table>";
+
+    container.append(content);
+
+    return container;
+}
+
+function keywordElement(keyw, freq) {
+    var container = document.createElement("a"); 
+    container.classList = "keyword keyword-" + freq;
+    container.setAttribute("href", "https://hal.science/search/?q=*&authIdHal_s=" + idhal + "&keyword_s=" + keyw);
+    container.setAttribute("target","_blank")
+    container.innerText = keyw; 
+    return container;
 }
 
 function genListPubli(type) {
-    callHALAPI(type).then(data => {
+    const param = {
+        fl: [
+            "title_s",
+            "halId_s",
+            "citationRef_s",
+            "defenseDateY_i",
+            "journalTitle_s",
+            "authFullName_s",
+            "publicationDate_tdate",
+            "fileMain_s", 
+            "thumbId_i"
+        ],
+        fq: "docType_s:" + type,
+        sort: "publicationDate_tdate desc", 
+    }
+    callHALAPI(param).then(data => {
         // Set title 
         document.getElementById("hal-" + type + "-card-title").innerText = hal_helpers[type]["title_en"] + " (" + data.length + ")";
 
@@ -121,12 +216,18 @@ function genListPubli(type) {
             str = str +
                 "<div class='citation-results'>" +
                 p.citationRef_s +
-                "</div>" +
-                "</td>" +
+                "</div>";
+            str = str +
+                "<div class='export-result'>";
+            if (p.thumbId_i) {
+                str = str + "<a href='" + p.fileMain_s  + "' target='_blank' class='hal-export-pdf'><i class='fa-regular fa-file-pdf'></i></a>";
+            }   
+            str = str + "<a href='https://hal.science/" + p.halId_s  + "/bibtex' title='bibtex' target='_blank' class='hal-export-cite'><i class='fa-solid fa-quote-right'></i></a>" + 
+                "</div>";
+            str = str + "</td>" +
                 " </tr>";
             tab.innerHTML += str;
         };
-
 
         // Remove loader
         document.getElementById("hal-" + type + "-spinner").style.display = "none";
@@ -137,30 +238,63 @@ function genListPubli(type) {
     }).catch(error => console.error(error));
 }
 
+function create_hal_publication_list() {
+    // Récupère la div ou placer la liste des publications 
+    var hal_publi_div = document.getElementById("publi-hal-all");
+
+    // Si elle n'existe pas on ne fait rien 
+    if (hal_publi_div.length === 0) {
+        if ((typeof halDebug !== "undefined") && (halDebug)) {
+            console.log("No HAL publication div on this page");
+        }
+        return 0;
+    } else {
+        if ((typeof halDebug !== "undefined") && (halDebug)) {
+            console.log("Create a list of publications in ");
+            console.log(hal_publi_div);
+        }
+    }
+
+    // Sinon pour chaque type de publication on créé la liste
+    for (const p of publiList) {
+        hal_publi_div.appendChild(initialHTML(p));
+        genListPubli(p);
+    }
+}; 
+
+
+function create_hal_wordcloud() {
+    // Récupède la div ou placer le nuage de mots
+    var hal_wordcloud_div = document.getElementById("wordcloud-hal");
+
+    // Si elle n'existe pas on ne fait rien 
+    if (hal_wordcloud_div.length === 0) {
+        if ((typeof halDebug !== "undefined") && (halDebug)) {
+            console.log("No HAL wordcloud div on this page");
+        }
+        return 0;
+    } else {
+        if ((typeof halDebug !== "undefined") && (halDebug)) {
+            console.log("Create wordcloud of keywords in ");
+            console.log(hal_wordcloud_div);
+        }
+    }
+
+    // Sinon on créé le nuage de mot
+    HALwordcloud(hal_wordcloud_div);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     if ((typeof halDebug !== "undefined") && (halDebug)) {
         console.log(idhal);
         console.log(publiList);
     }
-    const hal_div = document.getElementById("publi-hal-all");
 
-    if (hal_div.length === 0) {
-        if ((typeof halDebug !== "undefined") && (halDebug)) {
-            console.log("No Hal on this page");
-        }
-        return 0;
-    }
-
-    for (const p of publiList) {
-        // console.log("Add initial html for " + p);
-        // initialisation 
-        hal_div.innerHTML += initialHTML(p);
-
-        // generate publi list 
-        genListPubli(p);
-    }
+    create_hal_publication_list(); 
+    create_hal_wordcloud();
 });
 
+// Fonction pour collapser les sections de publications en cliquant sur leur titre
 function collapse(elem) {
     const elem2 = document.getElementById(elem.getAttribute("data-target").replace('#', ''));
     if (elem2.style.display === "none") {
