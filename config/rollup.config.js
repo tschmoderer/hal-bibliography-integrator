92a50677692f820c318c82b60b66593efb392170
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2023-06-16 10:21:48 +0300

    Update rollup.config.js

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
 
-export default {
-    input: path.resolve(__dirname, '../src/js/main.js'),
+export default [
+    // MAIN 
+    {
+        input: path.resolve(__dirname, '../src/js/main.js'),
 
-    output: [
-        {
-            file: path.resolve(__dirname, "../dist/hal.js"),
-            format: 'cjs',
+        output: [
+            {
+                file: path.resolve(__dirname, "../dist/hal.js"),
+                format: 'cjs',
+            },
+        ],
+
+        watch: {
+            include: path.resolve(__dirname, "../src/js/*")
         },
-    ],
 
-    watch: {
-        include: path.resolve(__dirname, "../src/js/*")
-    },
+        treeshake: false,
+    }, 
+    
+    // WORDCLOUD
+    {
+        input: path.resolve(__dirname, '../plugins/wordcloud/js/main.js'),
 
-    treeshake: false,
-};
+        output: [
+            {
+                file: path.resolve(__dirname, "../dist/plugings/wordcloud/hal-wordcloud.js"),
+                format: 'cjs',
+            },
+        ],
+
+        watch: {
+            include: path.resolve(__dirname, "../plugins/wordcloud/js/*")
+        },
+
+        treeshake: false,
+    }
+];
