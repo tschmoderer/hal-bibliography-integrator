c4e688cc495c1512538d5dfae7b38780da6f9dd4
iathena <timothee.schmoderer@netcourrier.com>
2026-04-13 15:58:58 +0200

    Update vite.config.demo.js

--- config/vite.config.demo.js
+++ config/vite.config.demo.js
@@ -8,18 +8,5 @@ export default defineConfig({
     build: {
         outDir: resolve(__dirname, '../docs'),
         emptyOutDir: true,
-
-        // rollupOptions: {
-        //     input: {
-        //         main: resolve(__dirname, '../demo/index.html'),
-        //     },
-        // },
     },
-
-    // // 5. Ensure your demo can resolve the library source code
-    // resolve: {
-    //     alias: {
-    //         '@lib': resolve(__dirname, '../src/js/main.js'),
-    //     },
-    // },
 });
